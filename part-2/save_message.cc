0daba4ac40d9c4561c768b51df2cd0f2d8c5a4b1
Espinoza <gray@cs408u-1f.acad.fullerton.edu>
2022-10-27 15:11:44 -0700

    wrote entire program

--- part-2/save_message.cc
+++ part-2/save_message.cc
@@ -1,5 +1,14 @@
-
-// TODO: Insert your header
+// Grey Espinoza
+// CPSC 120-12
+// 2022-10-27
+// gray@csu.fullerton.edu
+// @CSUFgrey
+//
+// Lab 08-02
+// Partners: @brian-250
+//
+// Program to output a file from input
+//
 
 #include <fstream>
 #include <iostream>
@@ -7,36 +16,31 @@
 #include <vector>
 
 void Prompt() {
-  // TODO: Write a function that prints to std::cout the following prompt and
-  // instructions: "What's a secret message that you'd like to write into a
-  // file?\n" "Type your message out and when you're done press return or
-  // enter.\n";
+  std::cout
+      << "What's a secret message that you'd like to write into a file?\n"
+      << "Type your message out and when you're done press return or enter.\n";
 }
 
 int main(int argc, char* argv[]) {
-  // TODO: Convert argv to a vector of arguments.
-  // TODO: Check if the number of arguments is enough to continue.
-  // If there are too few arguments, print a descriptive error message
-  // and exit.
-  // TODO: Declare a std::string variable named output_file_name.
-  // Initialize this variable to the first command line argument.
-  // TODO: Declare a std::ofstream variable named output_file_stream.
-  // Initialize the variable using the variable output_file_name.
-  // TODO: Check to see if output_file_stream is open. If output_file_stream
-  // is not open, then print a descriptive error message and exit.
-  // See https://en.cppreference.com/w/cpp/io/basic_ofstream/is_open
-  // TODO: Declare a std::string variable named secret_message.
-  // TODO: Call the function Prompt() to print out the prompt and instructions.
-  // TODO: Use std::getline() to read from std::cin and store the line into
-  // the variable secret_message.
-  // TODO: Use the insertion operator (<<) to send the value of secret_message
-  // to
-  //  output_file_stream. Remember to end the line that is written to
-  //  output_file_stream
-  // with a new line ("\n").
-  // TODO: Print a message that the secret message was saved into
-  // output_file_name. Make sure your output matches the sample output in the
-  // README.
-  // TODO: Close output_file_stream.
+  std::vector<std::string> arguments =
+      std::vector<std::string>{argv, argv + argc};
+  if (arguments.size() != 2) {
+    std::cout << "Please provide a path to a file.\n";
+    return -1;
+  }
+  std::string output_file_name{arguments.at(1)};
+  std::ofstream output_file_stream{output_file_name};
+  if (!output_file_stream.is_open()) {
+    std::cout << "Could not open the file " << output_file_name
+              << ". Exiting.\n";
+    return -1;
+  }
+  std::string secret_message{};
+  Prompt();
+  std::getline(std::cin, secret_message);
+  output_file_stream << secret_message << "\n";
+  std::cout << "Your secret message was saved into " << output_file_name
+            << "\n";
+  output_file_stream.close();
   return 0;
 }
